fb94892b0cd2338f5d4e6b3486b9a69d992b6629
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-06-09 14:48:28 +0100

    add sql into src/main and src/test

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,8 +1,8 @@
-drop schema ims;
+DROP DATABASE IF EXISTS ims;
 
-CREATE SCHEMA IF NOT EXISTS `ims`;
+CREATE DATABASE IF NOT EXISTS ims;
 
-USE `ims` ;
+USE ims ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -10,3 +10,28 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `itemsid` INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    `itemsName` VARCHAR(40) NOT NULL,
+    `price` DEC(10, 2) NOT NULL,
+    PRIMARY KEY (`itemsid`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+      order_id INT NOT NULL AUTO_INCREMENT, 
+      fk_id INT NOT NULL,
+      PRIMARY KEY (order_id),
+      FOREIGN KEY (fk_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+
+   CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
+      order_itemsID INT NOT NULL AUTO_INCREMENT,
+      fk_order_id INT NOT NULL,
+      fk_itemsid INT NOT NULL,
+      quantity INT DEFAULT 1,
+      PRIMARY KEY (order_itemsID),
+      FOREIGN KEY (fk_order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+      FOREIGN KEY (fk_itemsid) REFERENCES items(itemsid) ON DELETE CASCADE
+); 
+
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `orders_items`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -6,3 +9,27 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `itemsid` INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    `itemsName` VARCHAR(40) NOT NULL,
+    `price` DEC(10, 2) NOT NULL,
+    PRIMARY KEY (`itemsid`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+      order_id INT NOT NULL AUTO_INCREMENT, 
+      fk_id INT NOT NULL,
+      PRIMARY KEY (order_id),
+      FOREIGN KEY (fk_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+
+   CREATE TABLE IF NOT EXISTS `orders_items` (
+      order_itemsID INT NOT NULL AUTO_INCREMENT,
+      fk_order_id INT NOT NULL,
+      fk_itemsid INT NOT NULL,
+      quantity INT DEFAULT 1,
+      PRIMARY KEY (order_itemsID),
+      FOREIGN KEY (fk_order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+      FOREIGN KEY (fk_itemsid) REFERENCES items(itemsid) ON DELETE CASCADE
+); 
