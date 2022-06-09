9451cee102e64e85fd242de7e748e28670d88910
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-06-09 19:27:01 +0100

    update item controller test

--- src/test/java/com/qa/ims/controllers/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -6,15 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.controller.ItemController;
 import com.qa.ims.persistence.dao.ItemDAO;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.Utils;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ItemControllerTest {
 	
 	@Mock
